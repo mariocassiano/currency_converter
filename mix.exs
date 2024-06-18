e9aa0dc6552dc4e290a17d189e6f91979bf44d57
Mario Cassiano <mariocassiano@gmail.com>
2024-06-18 01:09:46 -0300

    chore: add excoveralls

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule CurrencyConverter.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.18", only: :test}
     ]
   end
 

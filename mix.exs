4fafc20c34ed7768cc5ac1e92dfcdfc4f99d42af
Mario Cassiano <mariocassiano@gmail.com>
2024-06-19 08:27:38 -0300

    fix: buildpacks

--- mix.exs
+++ mix.exs
@@ -67,13 +67,7 @@ defmodule CurrencyConverter.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["esbuild.install --if-missing"],
-      "assets.build": ["esbuild currency_converter"],
-      "assets.deploy": [
-        "esbuild currency_converter --minify",
-        "phx.digest"
-      ]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
   end
 end

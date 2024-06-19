b5275da4e275068259cd17c8e3c20108ed27388f
Mario Cassiano <mariocassiano@gmail.com>
2024-06-19 10:31:47 -0300

    categories

---
+++ lib/currency_converter_web/controllers/category_json.ex
@@ -0,0 +1,25 @@
+defmodule CurrencyConverterWeb.CategoryJSON do
+  alias CurrencyConverter.Categories.Category
+
+  @doc """
+  Renders a list of categories.
+  """
+  def index(%{categories: categories}) do
+    %{data: for(category <- categories, do: data(category))}
+  end
+
+  @doc """
+  Renders a single category.
+  """
+  def show(%{category: category}) do
+    %{data: data(category)}
+  end
+
+  defp data(%Category{} = category) do
+    %{
+      id: category.id,
+      name: category.name,
+      description: category.description
+    }
+  end
+end
--- lib/currency_converter_web/router.ex
+++ lib/currency_converter_web/router.ex
@@ -7,6 +7,8 @@ defmodule CurrencyConverterWeb.Router do
 
   scope "/api", CurrencyConverterWeb do
     pipe_through :api
+
+    resources "/categories", CategoryController, except: [:new, :edit]
   end
 
   # Enable LiveDashboard in development
